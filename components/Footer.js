bf2dd435812d4aab615ae413e0292cb1c654226a
shawnaydelotte <50062516+shawnaydelotte@users.noreply.github.com>
2022-04-23 19:34:20 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Forked and modified by me from netlify and next.js for you (and me) \n MMXXII
+        Forked and modified by me from netlify and next.js for you (and me)
+MMXXII
       </footer>
     </>
   )
