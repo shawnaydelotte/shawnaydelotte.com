3daea1221370b4bb609081c3b24dbaac60cba6b8
shawnaydelotte <50062516+shawnaydelotte@users.noreply.github.com>
2022-04-23 18:10:17 -0400

    Update index.js
    custom text.

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Words and Phrases</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Words and Phrases" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Notes and Thoughts <code>const expressedAs = "code";</code>
         </p>
       </main>
 
