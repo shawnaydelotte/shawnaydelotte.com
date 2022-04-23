fffcb96daa88896716d00aa9f28b14f3b0111284
shawnaydelotte <50062516+shawnaydelotte@users.noreply.github.com>
2022-04-23 18:12:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Words and Phrases" />
         <p className="description">
-          Notes and Thoughts <code>const expressedAs = "code";</code>
+          Notes on processes <code>const expressedAs = "code";</code>
         </p>
       </main>
 
