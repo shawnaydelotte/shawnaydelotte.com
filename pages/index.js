e26bb82314557314144444dd84bea5740e8b7146
shawnaydelotte <50062516+shawnaydelotte@users.noreply.github.com>
2022-04-26 17:39:59 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Words and Phrases" />
+
         <p className="description">
           Notes on processes
         </p>
