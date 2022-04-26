5da40a9029ba1c3233eeebdbde7c5aa3bb244428
shawnaydelotte <50062516+shawnaydelotte@users.noreply.github.com>
2022-04-26 16:58:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Shawn Aydelotte</title>
+        <title>ShawnAydelotte.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
